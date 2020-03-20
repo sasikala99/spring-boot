88bbe9b67d3aa50e768e3c49585d07b2117a1b2f
16771 <16771@OESLCHN-BD81.OESLCHENNAI.COM>
2020-03-20 14:04:32 +0530

    initial commit

---
+++ spring-thymeleaf/src/main/java/com/springthymeleaf/persistance/Student.java
@@ -0,0 +1,119 @@
+package com.springthymeleaf.persistance;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "studentdetails")
+public class Student implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@NotBlank(message = "Name is mandatory")
+	private String name;
+
+	@NotBlank(message = "Email is mandatory")
+	private String email;
+
+	private String phoneNo;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(long id, @NotBlank(message = "Name is mandatory") String name,
+			@NotBlank(message = "Email is mandatory") String email, String phoneNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phoneNo = phoneNo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phoneNo == null) ? 0 : phoneNo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phoneNo == null) {
+			if (other.phoneNo != null)
+				return false;
+		} else if (!phoneNo.equals(other.phoneNo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", phoneNo=" + phoneNo + "]";
+	}
+	
+	
+}
---
+++ spring-thymeleaf/src/main/java/com/springthymeleaf/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.springthymeleaf.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springthymeleaf.persistance.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	 List<Student> findByName(String name);
+}
