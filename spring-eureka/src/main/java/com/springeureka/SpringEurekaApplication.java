22f83ff961281e778ca59d282e2cd31e5c715bab
16771 <16771@OESLCHN-BD81.OESLCHENNAI.COM>
2020-03-20 10:27:48 +0530

    initial commit

---
+++ spring-eureka/src/main/java/com/springeureka/SpringEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class SpringEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaApplication.class, args);
+	}
+
+}
